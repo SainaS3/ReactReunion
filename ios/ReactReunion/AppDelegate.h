99a17d13bbb8dee719f80fba68f995cd855b6d97
VOM7HC <20337876+VOM7HC@users.noreply.github.com>
2024-07-17 17:31:45 +0700

    Init Project

--- ios/ReactReunion/AppDelegate.h
+++ ios/ReactReunion/AppDelegate.h
@@ -1,6 +1,9 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
+// Include a C++ header to ensure GitHub recognizes this as Objective-C++
+#include <vector>
+
 @interface AppDelegate : RCTAppDelegate
 
 @end
